7a2cb21c085d590f1f3bde12171c4266e72f3436
AshTheDergy <139553561+AshTheDergy@users.noreply.github.com>
2023-12-19 23:32:33 +0200

    nyah

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -9,32 +9,24 @@ const { PermissionFlagsBits } = require("discord.js");
 client.on("messageCreate", async (message) => {
 
     if (message.author.id !== client.user.id &&
-        message.author.bot &&
+        //message.author.bot &&
         client.config.categories.includes(message.channel.parentId) &&
         client.config.blacklistedWords.some((word) =>
-    new RegExp(
-      `\\b${word
-        .split('')
-        .join('\\s*\\W*')
-        .replace(/\s/g, '\\s*')}\\b`,
-      'gi'
-    ).test(message.content)
-  ))
-        {
+        new RegExp(
+            `\\b${word
+              .split('')
+              .join('\\s*[\\W_]*')
+              .replace(/\s/g, '\\s*')}\\b`,
+            'gi'
+        ).test(message.content)))
+    {
 
         const chnl = client.channels.cache.get("1186738350053400676") //client.channels.cache.get(client.config.channel);
-        const word = client.config.blacklistedWords.filter((word) =>
-    new RegExp(
-      `\\b${word
-        .split('')
-        .join('\\s*\\W*')
-        .replace(/\s/g, '\\s*')}\\b`,
-      'gi'
-    ).test(message.content)
-  );
-            chnl.send(
+        const word = client.config.blacklistedWords.find(word => message.content.toLowerCase().includes(word.toLowerCase()));
+
+        chnl.send(
             `
-            **User**: ***${message.author.username}***\n**In ${message.channel}** [(link to message)](https://discord.com/channels/${message.guildId}/${message.channel.id}/${message.id})\n**Message:**\n\`${message.content}\`\n**Triggered word(s):** \`${word.join(', ')}\`
+            **User**: ***${message.author.username}***\n**In ${message.channel}** [(link to message)](https://discord.com/channels/${message.guildId}/${message.channel.id}/${message.id})\n**Message:**\n\`${message.content}\`\n**Triggered word:** \`${word}\`
             `
         );
         return;
@@ -97,4 +89,3 @@ client.on("messageCreate", async (message) => {
 function escapeRegex(newprefix) {
     return newprefix.replace(/[.*+?^${}()|[\]\\]/g, `\\$&`);
 }
-//nice :)
